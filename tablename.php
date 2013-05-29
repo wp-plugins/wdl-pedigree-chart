b2ee881ce41fb022e8cb794a7a382aad13dfb3d7
wdlyons <wdlyons@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-29 04:35:07 +0000

    Update Conditions
    git-svn-id: https://plugins.svn.wordpress.org/wdl-pedigree-chart/trunk@719713 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tablename.php
+++ tablename.php
@@ -1,8 +1,97 @@
 <?php
+
 	global $wpdb;
+
 	
+
 	/* Set the tablenames */
+
 	$table_name = $wpdb->prefix . "wdl_pedigree_person";
+
 	$table_name2 = $wpdb->prefix . "wdl_pedigree_marriage";
+
+	$table_name3 = $wpdb->prefix . "wdl_pedigree_css";
+
+
+
+	/* Set the default variable for the shortcode tables*/
+
+ 	$table_heading_ft_fam = "Tahoma, Geneva, sans-serif";
+
+	$table_heading_ft_size = "14";
+
+	$table_heading_ft_wght = "bold";
+
+	$table_heading_ft_col = "000000";
+
+	$table_heading_ft_style = "normal";
+
+	$insert_number = "1";
+
 	
+
+	$tables_th_ft_fam = "Tahoma, Geneva, sans-serif";
+
+	$tables_th_ft_size = "12";
+
+	$tables_th_ft_wght = "bold";
+
+	$tables_th_ft_col = "ffffff";
+
+	$tables_th_tx_trans = "uppercase";
+
+	$tables_th_bkgrd = "666";
+
+	
+
+	$tables_tr_ft_fam = "Tahoma, Geneva, sans-serif";
+
+	$tables_tr_ft_size = "13";
+
+	$tables_tr_ft_col = "000000";
+
+	$tables_tr_bkgrd = "ffffff";
+
+	$zebra_col = "F2F2F2";
+
+	
+
+	$link_ft_fam = "Tahoma, Geneva, sans-serif";
+
+	$link_ft_size = "13";
+
+	$link_ft_style = "normal";
+
+	$link_tx_dec = "none";
+
+	$link_tx_col = "000000"; 
+
+	$link_hov_col = "EEA100";
+
+	
+
+	$sibling_tb_width = "100%";
+
+	$sibling_tb_marg_left = "auto";
+
+	$sibling_tb_marg_right = "auto";
+
+	$spouse_tb_width = "100%";
+
+	$spouse_tb_marg_left = "auto";
+
+	$spouse_tb_marg_right = "auto";
+
+	$children_tb_width = "100%";
+
+	$children_tb_marg_left = "auto";
+
+	$children_tb_marg_right = "auto";
+
+	
+
+	
+
+
+
 ?>
